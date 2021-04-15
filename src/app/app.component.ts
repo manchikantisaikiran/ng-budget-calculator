f48d1f273d37c2b3bb6219eb03e47ffba6d78baa
manchikantisaikiran <manchikantisaikiran777@gmail.com>
2021-04-15 10:33:26 +0530

    budget calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BudgetService } from './budget.service';
+import { Spends } from './spends'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'budget-calculator';
+  // currentRoute;
+  updatedSpends: Spends[] = []
+  constructor(private budgetService: BudgetService) {
+    // this.currentRoute = this.router.url
+    // console.log(this.currentRoute)
+  }
+
+  updatedSpendsFunc() {
+    return this.updatedSpends = this.budgetService.SpendsArray
+  }
+
+  getCurrentRoute() {
+    console.log(this.budgetService.getCurrentRoute());
+    return this.budgetService.getCurrentRoute() !== '/'
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BudgetComponent } from './budget/budget.component';
+import { ProvideBudgetComponent } from './main/provide-budget/provide-budget.component';
+import { DisplayBudgetComponent } from './main/display-budget/display-budget.component';
+import { EditFormComponent } from './edit-form/edit-form.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BudgetComponent,
+    ProvideBudgetComponent,
+    DisplayBudgetComponent,
+    EditFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
